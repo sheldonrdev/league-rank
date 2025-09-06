349e6515f45ca901b60099f5cdcef3900ab9f23f
Sheldon Reddy <sheldon.reddy@gmail.com>
2025-09-06 16:42:19 +0200

    Adds League Table interface

---
+++ LeagueRankApp/LeagueRankApp/Core/ILeagueTable.cs
@@ -0,0 +1,22 @@
+﻿using LeagueRankApp.Models.Outgoing;
+
+namespace LeagueRankApp.Core;
+
+/// <summary>
+/// Manages the league table and team rankings.
+/// </summary>
+public interface ILeagueTable
+{
+    /// <summary>
+    /// Add a game result to the league table.
+    /// </summary>
+    /// <param name="result">Game result</param>
+    /// <exception cref="FormatException">Invalid result</exception>
+    void AddGameResult(string result);
+    
+    /// <summary>
+    /// Get teams ranked by points.
+    /// </summary>
+    /// <returns>List of Ranked Teams</returns>
+    List<RankedTeam> GetRankedTeams();
+}
