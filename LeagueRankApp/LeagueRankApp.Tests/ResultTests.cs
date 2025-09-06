8c735da632833c42cd5f1a66e4ba030a3a591bbe
Sheldon Reddy <sheldon.reddy@gmail.com>
2025-09-06 16:07:18 +0200

    Adds TDD tests for the league table

---
+++ LeagueRankApp/LeagueRankApp.Tests/ResultTests.cs
@@ -0,0 +1,54 @@
+﻿namespace LeagueRankApp.Tests;
+
+public class ResultTests
+{
+    [Fact]
+    public void AddGameResult_TeamWins_Awards3Points()
+    {
+        // Arrange
+       // var league = new LeagueTable();
+        
+        // Act & Assert
+        Assert.True(true, "TODO");
+    }
+
+    [Fact]
+    public void AddGameResult_GameDraw_Awards1PointEach()
+    {
+        // Arrange
+        //var league = new LeagueTable();
+        
+        // Act & Assert
+        Assert.True(true, "TODO");
+    }
+
+    [Fact]
+    public void AddGameResult_TeamLoses_Awards0Points()
+    {
+        // Arrange
+       // var league = new LeagueTable();
+        
+        // Act & Assert
+        Assert.True(true, "TODO");
+    }
+
+    [Fact]
+    public void GetRankedTeams_SortsByPointsDescendingThenAlphabeticallyByName_ReturnsSortedByPoints()
+    {
+        // Arrange
+        //var league = new LeagueTable();
+        
+        // Act & Assert
+        Assert.True(true, "TODO");
+    }
+
+    [Fact]
+    public void GetRankedTeams_TeamsWithSamePoints_HaveSameRank()
+    {
+        // Arrange
+       // var league = new LeagueTable();
+        
+        // Act & Assert
+        Assert.True(true, "TODO");
+    }
+}
