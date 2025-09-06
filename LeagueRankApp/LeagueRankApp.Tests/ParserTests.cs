6f1ca95698256642fda119043612ddfecb2f7305
Sheldon Reddy <sheldon.reddy@gmail.com>
2025-09-06 16:02:30 +0200

    Adds tests for sample input game results

--- LeagueRankApp/LeagueRankApp.Tests/ParserTests.cs
+++ LeagueRankApp/LeagueRankApp.Tests/ParserTests.cs
@@ -17,4 +17,30 @@ public class ParserTests
         Assert.Equal("Snakes", result.TeamTwo.Name);
         Assert.Equal(3, result.TeamTwo.Score);
     }
+    
+    [Theory]
+    [InlineData("Lions 3, Snakes 3", "Lions", 3, "Snakes", 3)]
+    [InlineData("Tarantulas 1, FC Awesome 0", "Tarantulas", 1, "FC Awesome", 0)]
+    [InlineData("Lions 1, FC Awesome 1", "Lions", 1, "FC Awesome", 1)]
+    [InlineData("Tarantulas 3, Snakes 1", "Tarantulas", 3, "Snakes", 1)]
+    [InlineData("Lions 4, Grouches 0", "Lions", 4, "Grouches", 0)]
+    public void ParseGameResult_ValidInputs_ReturnsCorrectTeamNamesAndScores(
+        string input,
+        string expectedTeamOneName,
+        int expectedTeamOneScore,
+        string expectedTeamTwoName,
+        int expectedTeamTwoScore)
+    {
+        // Arrange
+        var parser = new GameResultParser();
+    
+        // Act
+        var result = parser.ParseGameResult(input);
+    
+        // Assert
+        Assert.Equal(expectedTeamOneName, result.TeamOne.Name);
+        Assert.Equal(expectedTeamOneScore, result.TeamOne.Score);
+        Assert.Equal(expectedTeamTwoName, result.TeamTwo.Name);
+        Assert.Equal(expectedTeamTwoScore, result.TeamTwo.Score);
+    }
 }
