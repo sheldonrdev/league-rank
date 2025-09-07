10697104a317ad343232f80c5e29b32e85be1bfd
Sheldon Reddy <sheldon.reddy@gmail.com>
2025-09-07 08:25:21 +0200

    Adds AddGameResult implementation

--- LeagueRankApp/LeagueRankApp/Core/LeagueTable.cs
+++ LeagueRankApp/LeagueRankApp/Core/LeagueTable.cs
@@ -1,3 +1,4 @@
+using LeagueRankApp.Models.Incoming;
 using LeagueRankApp.Models.Outgoing;
 
 namespace LeagueRankApp.Core;
@@ -11,7 +12,29 @@ public class LeagueTable : ILeagueTable
    
     public void AddGameResult(string result)
     {
-        throw new NotImplementedException();
+        var gameResult = _resultParser.ParseGameResult(result);
+
+        AddTeamToLeague(gameResult.TeamOne.Name);
+        AddTeamToLeague(gameResult.TeamTwo.Name);
+
+        var team1 = _teams[gameResult.TeamOne.Name];
+        var team2 = _teams[gameResult.TeamTwo.Name];
+
+        if (gameResult.TeamOne.Score > gameResult.TeamTwo.Score)
+        {
+            team1.AddWin();
+            team2.AddLoss();
+        }
+        else if (gameResult.TeamTwo.Score > gameResult.TeamOne.Score)
+        {
+            team2.AddWin();
+            team1.AddLoss();
+        }
+        else
+        {
+            team1.AddDraw();
+            team2.AddDraw();
+        }
     }
 
     public List<RankedTeam> GetRankedTeams()
