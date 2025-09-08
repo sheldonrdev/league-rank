affd7c4f3f52711ec1dbf4a2c3bdae6c33b3ff50
Sheldon Reddy <sheldon.reddy@gmail.com>
2025-09-08 21:31:12 +0200

    Adds performance improvement to file read operations

--- LeagueRankApp/LeagueRankApp/Core/Util/FileInputReader.cs
+++ LeagueRankApp/LeagueRankApp/Core/Util/FileInputReader.cs
@@ -3,10 +3,10 @@ namespace LeagueRankApp.Core.Util;
 public static class FileInputReader
 {
     /// <summary>
-    /// Read lines from a file.
+    /// Read lines from a file using streaming.
     /// </summary>
     /// <param name="inputFile">Path to input file</param>
-    /// <returns>Enumerable of input lines</returns>
+    /// <returns>Enumerable of input lines streamed line-by-line from file</returns>
     public static IEnumerable<string> ReadLines(string inputFile)
     {
         if (string.IsNullOrWhiteSpace(inputFile))
@@ -15,6 +15,16 @@ public static class FileInputReader
         if (!File.Exists(inputFile))
             throw new FileNotFoundException($"Input file not found: {inputFile}");
         
-        return File.ReadAllLines(inputFile);
+        return StreamLines(inputFile);
+    }
+    
+    private static IEnumerable<string> StreamLines(string inputFile)
+    {
+        using var reader = new StreamReader(inputFile);
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            yield return line;
+        }
     }
 }
